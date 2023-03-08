5311bb85f305c6b07e1adc736c96e74ee912d9bc
VSciFlight <amartinb20001@gmail.com>
2023-03-08 18:31:41 +0200

    login, signups, locators

--- locators.py
+++ locators.py
@@ -2,7 +2,6 @@ import utils as u
 
 class Locator:
 
-
     locHome = dict()
 
 
@@ -67,6 +66,10 @@ class Locator:
 
     locLog['Login_Username'] = (u.By.XPATH, '//*[@id="loginusername"]')
     locLog['Login_Password'] = (u.By.XPATH, '//*[@id="loginpassword"]')
+    locLog['Login_Button'] = (u.By.XPATH, '//*[@id="logInModal"]/div/div/div[3]/button[2]')
 
     locLog['Signup_Username'] = (u.By.XPATH, '//*[@id="sign-username"]')
     locLog['Signup_Password'] = (u.By.XPATH, '//*[@id="sign-password"]')
+    locLog['Signup_Button'] = (u.By.XPATH, '//*[@id="signInModal"]/div/div/div[3]/button[2]')
+
+    locLog['Welcome'] = (u.By.XPATH, '//*[@id="nameofuser"]')
