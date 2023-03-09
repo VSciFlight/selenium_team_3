8d4fa18e89c968a4991e3f77f8d308f98b4578b9
Nirpeled1 <nirpeled12@gmail.com>
2023-03-09 11:22:47 +0200

    dd

--- pages/cart.py
+++ pages/cart.py
@@ -16,4 +16,30 @@ def add_to_cart_button():
     driver.find_element(u.By.XPATH, '//*[@id="cartur"]').click()
     u.sleep(4)
 
-add_to_cart_button()
+########################################################################################################
+import utils as u
+import unittest
+
+
+class Test_Cart_Page(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = u.WebDriver.Chrome()
+
+    def test_add_to_cart_button(self):
+        self.driver.get("https://demoblaze.com/index.html")
+        u.sleep(3)
+        self.driver.find_element(u.By.XPATH, '//*[@id="tbodyid"]/div[1]/div/div/h4/a').click()
+        u.sleep(3)
+        self.add_cart = self.driver.find_element(u.By.XPATH, '//*[@id="tbodyid"]/div[2]/div/a')
+        self.add_cart.click()
+        u.sleep(3)
+        u.WDW(self.driver, 3).until(u.EC.alert_is_present())
+        self.alert = self.driver.switch_to.alert
+        self.alert.accept()
+        self.driver.find_element(u.By.XPATH, '//*[@id="cartur"]').click()
+        u.sleep(3)
+        self.assertTrue(True)
+
+    def tearDown(self):
+        self.driver.quit()
